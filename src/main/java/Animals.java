42fb6ab0062a70804982ef6bb09ffc7ee1393c2e
gilbertKorir <jerikorir@gmail.com>
2022-05-06 15:21:10 +0300

    ft: generated the getter methods in class animals

--- src/main/java/Animals.java
+++ src/main/java/Animals.java
@@ -6,5 +6,30 @@ public class Animals implements DatabaseManagement {
     public int id;
     public static final String ANIMAL_TYPE = "normal";
 
+    public Animals(String name, String type) {
+        this.name = name;
+        this.type = ANIMAL_TYPE;
+        this.age = "";
+        this.health = "";
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
