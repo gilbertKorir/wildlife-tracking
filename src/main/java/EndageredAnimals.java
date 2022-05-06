52d98bce04b0eac2346bc9d18d655c44bd707920
gilbertKorir <jerikorir@gmail.com>
2022-05-06 16:32:43 +0300

    ft: added a save method overriding other classes to save into animals table

--- src/main/java/EndageredAnimals.java
+++ src/main/java/EndageredAnimals.java
@@ -1,3 +1,5 @@
+import org.sql2o.Connection;
+
 public class EndageredAnimals extends Animals implements DatabaseManagement {
     private String health;
     private String age;
@@ -26,5 +28,26 @@ public EndageredAnimals(String name,String type,String health,String age){
     }
     public String getAge() {
         return age;
+    }
+    //save method
+    @Override
+    public void save() {
+        if(this.name.equals("")||this.type.equals("")||this.health.equals("")||this.age.equals("")){
+            throw new IllegalArgumentException("Fields cannot be empty");
+        }
+        try (Connection con=DB.sql2o.open()){
+
+
+            String sql ="INSERT INTO animals (name,type,health,age) VALUES (:name,:type,:health,:age)";
+
+            this.id=(int) con.createQuery(sql,true)
+                    .addParameter("name",this.name)
+                    .addParameter("type",this.type)
+                    .addParameter("health",this.health)
+                    .addParameter("age",this.age)
+                    .executeUpdate()
+                    .getKey();
+        }
+
     }
 }
