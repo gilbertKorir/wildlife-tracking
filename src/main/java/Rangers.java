bbf2e43d1012f177792865f8baf3cb7e82c62d2e
gilbertKorir <jerikorir@gmail.com>
2022-05-06 17:48:31 +0300

    ft: generated the properties and getters for class rangers

--- src/main/java/Rangers.java
+++ src/main/java/Rangers.java
@@ -1,2 +1,28 @@
 public class Rangers {
+    private String name;
+    private int id;
+    private String phone_number;
+    private String badge_number;
+
+    public Rangers(String name, String phone_number, String badge_number) {
+        this.name = name;
+        this.phone_number = phone_number;
+        this.badge_number = badge_number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public String getBadge_number() {
+        return badge_number;
+    }
 }
