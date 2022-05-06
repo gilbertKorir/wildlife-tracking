36ad253f6bc388659c582982514b29cf8b1c3716
gilbertKorir <jerikorir@gmail.com>
2022-05-06 16:23:57 +0300

    ft: generated arguments in the class endagered animals

--- src/main/java/EndageredAnimals.java
+++ src/main/java/EndageredAnimals.java
@@ -1,2 +1,20 @@
 public class EndageredAnimals {
+    private String health;
+    private String age;
+
+    //constants age
+    public static final String AGE_NEWBORN="newborn";
+    public static final String AGE_YOUNG="young";
+    public static final String AGE_ADULT="adult";
+
+    //constants health
+    public static final String HEALTH_HEALTHY="healthy";
+    public static final String HEALTH_ILL="ill";
+    public static final String HEALTH_OKAY="okay";
+
+    //constant type
+    public static final String ANIMAL_TYPE="endangered";
+public EndageredAnimals(String name,String type,String health,String age){
+
+}
 }
