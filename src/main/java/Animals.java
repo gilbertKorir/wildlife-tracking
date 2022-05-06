0e2becba94f0c43e85c2877ec480358457c35a83
gilbertKorir <jerikorir@gmail.com>
2022-05-06 16:05:40 +0300

    ft: added methods to update, delete and save the animal in tha database

--- src/main/java/Animals.java
+++ src/main/java/Animals.java
@@ -1,3 +1,7 @@
+import org.sql2o.Sql2oException;
+//import java.sql.Connection;
+import org.sql2o.Connection;
+
 public class Animals implements DatabaseManagement {
     public String name;
     public String health;
@@ -32,4 +36,103 @@ public class Animals implements DatabaseManagement {
     public int getId() {
         return id;
     }
+
+    // save method to save the animal
+    public void save() {
+        if (this.name.equals("") || this.type.equals("") || this.name.equals(null) || this.type.equals(null)) {
+            throw new IllegalArgumentException("Validate all the inputs");
+        }
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (name,type) VALUES (:name,:type)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("type", this.type)
+                    .executeUpdate()
+                    .getKey();
+
+        }
+    }
+
+    //update
+    public void update(int id, String type, String health, String age) {
+        try (Connection con = DB.sql2o.open()) {
+            if (type.equals("")) {
+                throw new IllegalArgumentException("All fields must be filled");
+            }
+            if (type == "endangered") {
+                if (health.equals("") || age.equals("")) {
+                    throw new IllegalArgumentException("All fields must be filled");
+                }
+                String sql = "UPDATE animals SET type=:type,health=:health,age=:age WHERE id=:id";
+                con.createQuery(sql)
+                        .addParameter("type", type)
+                        .addParameter("health", health)
+                        .addParameter("age", age)
+                        .addParameter("id", this.id)
+                        .executeUpdate();
+            } else {
+
+                String sql = "UPDATE animals SET type=:type,health=:health,age=:age WHERE id=:id";
+                con.createQuery(sql)
+                        .addParameter("type", type)
+                        .addParameter("health", "")
+                        .addParameter("age", "")
+                        .addParameter("id", this.id)
+                        .executeUpdate();
+            }
+
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public static Animals find(int id) {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM animals WHERE id=:id";
+            Animals animal = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetchFirst(Animals.class);
+            return animal;
+
+        }
+
+    }
+
+    public void delete() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "DELETE FROM animals WHERE id=:id";
+            con.createQuery(sql)
+                    .addParameter("id", this.id)
+                    .executeUpdate();
+
+        }
+    }
+
+    public static void deleteAll() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "DELETE FROM animals";
+            con.createQuery(sql)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
