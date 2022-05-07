608309c5f802e8e45ea7f1aa168b392d611ea867
gilbertKorir <jerikorir@gmail.com>
2022-05-07 10:12:00 +0300

    ft: created a class sightings with parameters and its constructor

--- src/main/java/Sightings.java
+++ src/main/java/Sightings.java
@@ -1,2 +1,20 @@
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class Sightings {
+    private int animal_id;
+    private int ranger_id;
+    private int location_id;
+    private int id;
+    private Date date = new Date();
+    private Timestamp time;
+
+    // constructor
+    public Sightings(int location_id, int ranger_id, int animal_id) {
+        this.location_id = location_id;
+        this.ranger_id = ranger_id;
+        this.animal_id = animal_id;
+        this.time = new Timestamp(date.getTime());
+    }
+
 }
